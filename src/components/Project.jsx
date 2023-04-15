d52d42ebdafa300061092f4cc399cfc2329f9b07
khareenp <khareenp1@gmail.com>
2023-04-15 11:57:01 -0400

    update link

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -25,7 +25,7 @@ const Project = () => {
       img: project3,
       name: "Country App",
       github_link: "https://github.com/khareenp/countriesApp",
-      live_link: "https://github.com/khareenp/countriesApp",
+      live_link: "https://countrykp.netlify.app",
     },
   ];
   return (
